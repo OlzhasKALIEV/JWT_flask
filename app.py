86fc5c33a838c500e3d46c854d40f48eee613018
OlzhasKALIEV <130698888+OlzhasKALIEV@users.noreply.github.com>
2023-10-02 12:59:10 +0600

    flask

--- app.py
+++ app.py
@@ -40,19 +40,21 @@ def register_users():
 
 @app.route("/login", methods=["POST"])
 def login():
-    username = request.json.get("username", None)
-    password = request.json.get("password", None)
-    if username != "test" or password != "test":
-        return jsonify({"msg": "Bad username or password"}), 401
-
-    access_token = create_access_token(identity=username)
-    return jsonify(access_token=access_token)
+    username = request.json.get("username")
+    password = request.json.get("password")
+    user = Users.query.filter_by(username=username).first()
+    if user:
+        if username == user.username and password == user.password:
+            access_token = create_access_token(identity=username)
+            return jsonify(access_token=access_token), 200
+        return jsonify({"message": "Wrong login or password"})
+    else:
+        return jsonify({"message": "Users not"})
 
 
 @app.route("/protected", methods=["GET"])
 @jwt_required()
 def protected():
-    # Access the identity of the current user with get_jwt_identity
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
 
@@ -62,7 +64,8 @@ def protected():
 def hello():
     # Access the identity of the current user with get_jwt_identity
     current_user = get_jwt_identity()
-    return jsonify({"dsfsd": "hello"}), 200
+    logged_in_as = current_user
+    return jsonify({"message": f"hello {logged_in_as}"}), 200
 
 
 if __name__ == '__main__':
